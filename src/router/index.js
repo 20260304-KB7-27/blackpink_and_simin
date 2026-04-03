b82e21773f186fe75cfd5a6f43f721c7685429a3
dodo5517 <kh9015548@gmail.com>
2026-04-03 17:47:16 +0900

    feat: 라우터에 Guestbook 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    { path: '/guestbook', component: () => import('@/pages/Guestbook.vue') },
+  ],
+});
 
-export default router
+export default router;
