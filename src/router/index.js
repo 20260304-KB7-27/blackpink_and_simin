89bf23f42bb4fd198a34d49fc897a84d68463c08
KHJ <jin3574@dgu.ac.kr>
2026-04-03 17:12:16 +0900

    [#1] docs: 팀 규칙 페이지 구현

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: '/',
+      redirect: '/rules', // /rules로 바로 이동 (merge 시 삭제)
+    },
+    {
+      path: '/rules',
+      component: () => import('@/pages/TeamRule.vue'),
+    },
+  ],
+});
 
-export default router
+export default router;
