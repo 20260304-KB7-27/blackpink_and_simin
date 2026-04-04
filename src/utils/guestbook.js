cd27aba0ad845c6b79f8d71661adb1ec7cdd2c85
dodo5517 <kh9015548@gmail.com>
2026-04-05 08:21:11 +0900

    feat: 방명록 API 요청 try-catch 에러 처리 적용

--- src/utils/guestbook.js
+++ src/utils/guestbook.js
@@ -6,12 +6,22 @@ const api = axios.create({
 
 // 방명록 전체 조회
 export const getGuestbook = async () => {
-  const res = await api.get('/guestbook');
-  return res.data;
+  try {
+    const res = await api.get('/guestbook');
+    return res.data;
+  } catch (err) {
+    console.error('방명록 조회 API 에러:', err);
+    throw err;
+  }
 };
 
 // 방명록 추가
 export const createGuestbook = async (data) => {
-  const res = await api.post('/guestbook', data);
-  return res.data;
+  try {
+    const res = await api.post('/guestbook', data);
+    return res.data;
+  } catch (err) {
+    console.error('방명록 추가 API 에러:', err);
+    throw err;
+  }
 };
